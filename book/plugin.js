05a6e02563bd12940da7d83b0c1cd89fcbf97900
mengao <mengao@gridsum.com>
2020-02-26 19:31:04 +0800

    feat: 增加loadin效果

--- book/plugin.js
+++ book/plugin.js
@@ -1,34 +1,35 @@
 require(["gitbook"], function(gitbook) {
-    window.timer = null;
-    function checkLoginStatus() {
-        if(window.timer)return;
-        window.timer = setInterval(function(){
-            var content = document.querySelector('#login-iframe').contentWindow;
-            content.postMessage({check: true}, '*');
-        }, 500);
+  window.timer = null;
+  function checkLoginStatus() {
+      if(window.timer)return;
+      window.timer = setInterval(function(){
+          var content = document.querySelector('#login-iframe').contentWindow;
+          content.postMessage({check: true}, '*');
+      }, 500);
+      document.getElementsByClassName('cover')[0].style.display='flex';
+      window.addEventListener('message', function(event){
+          var fullTicketId = '';
+          if (typeof event.data === 'string') {
+              try {
+                  fullTicketId = JSON.parse(event.data).fullTicketId;
+              } catch (e) {
+                  console.warn('本次 message 事件中未解析得到 fullTicketId ：', e);
+              }
+          }
+          if (event.data && fullTicketId) {
+              // 登录成功，清除定时器，不做其它操作
+              clearInterval(window.timer);
+              window.timer = null;
+              document.getElementsByClassName('cover')[0].style.display='none';
+          } else if (event.data && event.data.check ) {
+              // 登录失败，需要重定向至登录页面
+              window.location.href="/web/#login";
+              clearInterval(window.timer);
+          }
+      }, false);
+      window.__LISTENING_LOGIN_MESSAGE__ = true;
+  }
 
-        window.addEventListener('message', function(event){
-            var fullTicketId = '';
-            if (typeof event.data === 'string') {
-                try {
-                    fullTicketId = JSON.parse(event.data).fullTicketId;
-                } catch (e) {
-                    console.warn('本次 message 事件中未解析得到 fullTicketId ：', e);
-                }
-            }
-            if (event.data && fullTicketId) {
-                // 登录成功，清除定时器，不做其它操作
-                clearInterval(window.timer);
-                window.timer = null;
-            } else if (event.data && event.data.check ) {
-                // 登录失败，需要重定向至登录页面
-                window.location.href="/#login";
-                clearInterval(window.timer);
-            }
-        }, false);
-        window.__LISTENING_LOGIN_MESSAGE__ = true;
-    }
-
-    gitbook.events.bind("start", checkLoginStatus);
-    gitbook.events.bind("page.change", checkLoginStatus);
+  gitbook.events.bind("start", checkLoginStatus);
+  gitbook.events.bind("page.change", checkLoginStatus);
 });
--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 module.exports = {
-    book: {
-        assets: "./book",
-        js: [
-            "plugin.js"
-        ]
-    },
-	hooks: {
-		"page:before": function(page) {
-			// 加上iframe与login-frame通信，引用同域名下的iframe
-			var iframeContent = "<iframe style='display:none' id='login-iframe' src='/login-iframe.html' />";
-			page.content = iframeContent +page.content;
-			return page;
-		}
-	}
+  book: {
+      assets: "./book",
+      js: [
+          "plugin.js"
+      ]
+  },
+hooks: {
+  "page:before": function(page) {
+    // 加上iframe与login-frame通信，引用同域名下的iframe
+    var iframeContent = "<style>.cover{position:fixed;top:0;left:0;right:0;bottom:0;display:flex;align-items:center;justify-content:center;background:white;z-index:10000}.point{width:10px;height:10px;background:black;border-radius:50%;float:left;margin-left:5px;animation:myAnima 1.2s ease-in-out infinite;}.two{animation-delay: 200ms;}.three{animation-delay: 400ms;}@keyframes myAnima{from {opacity: 0.8}to{opacity: 0}}</style>";
+    iframeContent += "<div class='cover'><div class='point one'></div><div class='point two'></div><div class='point three'></div>";
+    iframeContent += "<iframe style='display:none' id='login-iframe' src='/web/login-iframe.html'/>";
+    page.content = iframeContent +page.content;
+    return page;
+  }
+}
 
 };
